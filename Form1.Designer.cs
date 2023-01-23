8afaf6b6fbc6deb1abfd1c2ce19f6c0498cd18a3
Omar <Omar@COMPUDEADRIN>
2023-01-23 08:40:26 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace clase4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clase4
+{
+
+    public partial class Form1 : Form
+    {
+        static Bitmap bmp;
+        static Graphics g;
+        private Point  ancho1, ancho2 , alto1, alto2;
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point a, b, c, d;
+            a = new Point(0, 0);
+            b = new Point(0, 100);
+            c = new Point(100, 100);
+            d = new Point(100, 0);
+
+            pictureBox1.Image = null;
+            double angle = double.Parse(textBox1.Text);
+            int Sx = (bmp.Width / 2);
+            int Sy = (bmp.Height / 2);
+            Render( a, b, c,d, angle, Sx,Sy);
+            Refresh();
+          
+
+        }
+        private void Render(Point a, Point b, Point c, Point d, double angle, int Sx, int Sy)
+        {
+            setLines();
+
+            PointF a2, b2,c2,d2;
+            
+            a2 = new PointF(Sx + a.X, Sy - a.Y);
+            b2 = new PointF(Sx + b.X, Sy - b.Y);
+            c2 = new PointF(Sx + c.X, Sy - c.Y);
+            d2 = new PointF(Sx + d.X, Sy - d.Y);
+            angle = angle * 3.1416 / 180;
+
+            a2.X = Sx + (float)((a.X * Math.Cos(angle)) - (a.Y * Math.Sin(angle)));
+            a2.Y = Sy - (float)((a.X * Math.Sin(angle)) + (a.Y * Math.Cos(angle)));
+            b2.X = Sx + (float)((b.X * Math.Cos(angle)) - (b.Y * Math.Sin(angle)));
+            b2.Y = Sy - (float)((b.X * Math.Sin(angle)) + (b.Y * Math.Cos(angle)));
+
+            c2.X = Sx + (float)((c.X * Math.Cos(angle)) - (c.Y * Math.Sin(angle)));
+            c2.Y = Sy - (float)((c.X * Math.Sin(angle)) + (c.Y * Math.Cos(angle)));
+            d2.X = Sx + (float)((d.X * Math.Cos(angle)) - (d.Y * Math.Sin(angle)));
+            d2.Y = Sy - (float)((d.X * Math.Sin(angle)) + (d.Y * Math.Cos(angle)));
+
+            g.DrawLine(Pens.Red, a2, b2);
+            g.DrawLine(Pens.Red, b2, c2);
+            g.DrawLine(Pens.Red, c2, d2);
+            g.DrawLine(Pens.Red, d2, a2);
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Point a, b, c, d;
+            a = new Point(-50, -50);
+            b = new Point(-50, 50);
+            c = new Point(50, 50);
+            d = new Point(50, -50);
+
+            pictureBox1.Image = null;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            int Sx = (bmp.Width / 2);
+            int Sy = (bmp.Height / 2);
+
+            double angle = double.Parse(textBox1.Text);
+            Render(a, b, c, d, angle, Sx, Sy);
+            Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Point a, b, c, d;
+            a = new Point(-50, -50);
+            b = new Point(-50, 50);
+            c = new Point(50, 50);
+            d = new Point(50, -50);
+
+            pictureBox1.Image = null;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+
+            double angle = double.Parse(textBox1.Text);
+            int Sx = (bmp.Width / 2 +50);
+            int Sy = (bmp.Height / 2)-50;
+            Render(a, b, c, d, angle, Sx, Sy);
+            Refresh();
+        }
+
+        public void setLines()
+        {
+            ancho1 = new Point(0, pictureBox1.Height / 2);
+            ancho2 = new Point(pictureBox1.Width, pictureBox1.Height / 2);
+            alto1 = new Point(pictureBox1.Width / 2, 0);
+            alto2 = new Point(pictureBox1.Width / 2, pictureBox1.Height);
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+
+            g.DrawLine(Pens.Yellow, ancho1, ancho2);
+            g.DrawLine(Pens.Yellow, alto1, alto2);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+           // setLines();
+
+            int Sx = pictureBox1.Width / 2;
+            int Sy = pictureBox1.Height / 2;
+
+             Point a, b, c, d;
+
+            a = new Point(0, 0);
+            b = new Point(0, 100);
+            c = new Point(100 , 100);
+            d = new Point(100, 0);
+            
+
+            Render(a, b, c, d, 0,Sx,Sy);
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
